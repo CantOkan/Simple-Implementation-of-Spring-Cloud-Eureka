24cdf6f7936724dde286eb3b8a5d3dfa3cdd5df0
CantOkan <canokan21997@gmail.com>
2022-09-27 02:24:32 +0300

    added Eureka Server and Client Side Load Balancing

--- student-service/src/main/java/com/university/studentservice/feignclients/AddressFeignClient.java
+++ student-service/src/main/java/com/university/studentservice/feignclients/AddressFeignClient.java
@@ -5,9 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "${address.service.url}",value = "address-feign-client",path = "/api/address")
+@FeignClient(value = "address-service",path = "/api/address")
 public interface AddressFeignClient {
 
+    // address-service is name of the registered service in eureka server
     @GetMapping("/getById/{id}")
     public AddressResponse getById(@PathVariable Integer id);
 }
